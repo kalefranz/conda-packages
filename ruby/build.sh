38e05df9cf84afc5b7b4e5d1c9d67e60ea034066
Kale Franz <kfranz@23andme.com>
2015-04-10 21:39:43 -0700

    add libffi to ruby

--- ruby/build.sh
+++ ruby/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -eu
-./configure --prefix=$PREFIX \
+./configure \
+        --prefix=$PREFIX \
         --enable-load-relative \
         --disable-install-doc \
         --enable-shared \
+        --with-opt-dir=$PREFIX \
+        --with-openssl-dir=$PREFIX \
+        --with-readline-dir=$PREFIX \
         --with-zlib-dir=$PREFIX
 make
 make install
