409486bacf8c4126564f7d51c99ede942d2ad352
Kale Franz <kfranz@23andme.com>
2015-04-10 20:32:12 -0700

    fix zlib

--- ruby/build.sh
+++ ruby/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -eu
-./configure --prefix=$PREFIX --enable-load-relative --disable-install-doc --enable-shared
+./configure --prefix=$PREFIX \
+        --enable-load-relative \
+        --disable-install-doc \
+        --enable-shared \
+        --with-zlib-dir=$PREFIX
 make
 make install
