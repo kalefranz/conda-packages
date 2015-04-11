18fdca2c46dcafee3df082ddab25cd5006be9f74
Kale Franz <kfranz@23andme.com>
2015-04-11 11:32:18 -0700

    libstdcpp getting version

--- libstdcpp/build.sh
+++ libstdcpp/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -eu
-mkdir -p $PREFIX/lib
-cp /usr/lib/libstdc++.so.6 $PREFIX/lib
 
 /sbin/ldconfig -p | grep stdc++
 
 strings /usr/lib/libstdc++.so.6 | grep LIBCXX
 
 cat /etc/redhat-release
+
+
+
+mkdir -p $PREFIX/lib
+cp /usr/lib/libstdc++.so.6 $PREFIX/lib
+
