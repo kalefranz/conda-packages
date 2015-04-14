15d246d4bb1e7d211a7a2d9ee42f1ea187f99e5d
Kale Franz <kfranz@23andme.com>
2015-04-13 20:48:36 -0700

    cmake

--- rabbitmq-c/build.sh
+++ rabbitmq-c/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -eu
 
-autoreconf -i
-./configure --prefix=$PREFIX
-make
-make install
+# autoreconf -i
+# ./configure --prefix=$PREFIX
+# make
+# make install
+
+
+cmake \
+    -DENABLE_SSL_SUPPORT=ON \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX ..
+cmake --build . --target install
