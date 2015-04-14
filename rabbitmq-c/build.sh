0d48dfed149124c9b7201e2421f2ab908629751d
Kale Franz <kfranz@23andme.com>
2015-04-13 22:07:06 -0700

    clean up

--- rabbitmq-c/build.sh
+++ rabbitmq-c/build.sh
@@ -1,18 +1,5 @@
 #!/bin/bash -eu
-
-# autoreconf -i
-# ./configure --prefix=$PREFIX
-# make
-# make install
-
-ls -al
-pwd
-
 mkdir build && cd build
-
-ls -al
-pwd
-
 cmake \
     -DENABLE_SSL_SUPPORT=ON \
     -DCMAKE_INSTALL_PREFIX=$PREFIX ..
