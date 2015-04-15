f2917e6476a4270f04d2c541b4a6b1004f27f161
Kale Franz <kfranz@23andme.com>
2015-04-14 23:30:02 -0700

    mysql

---
+++ mysql/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -eu
+cmake . \
+    -DBUILD_CONFIG=mysql_release \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_FIND_FRAMEWORK=LAST \
+    -DCMAKE_VERBOSE_MAKEFILE=ON \
+    -DINSTALL_INCLUDEDIR=include/mysql \
+    -DINSTALL_MANDIR=share/man \
+    -DINSTALL_INFODIR=share/info \
+    -DINSTALL_MYSQLSHAREDIR=share/mysql \
+    -DWITH_SSL=yes \
+    -DWITH_SSL=system \
+    -DSYSCONFDIR=$PREFIX/etc \
+    -DMYSQL_DATADIR=var/db/mysql \
+    -DDEFAULT_CHARSET=utf8 \
+    -DDEFAULT_COLLATION=utf8_general_ci
+    # -DWITH_EDITLINE=system \
+    # -DINSTALL_DOCDIR=share/doc/mysql
+
+make
+make install
+
