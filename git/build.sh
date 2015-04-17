c733f772bc350be3af134ef3e7cc94b7c59e8d24
Kale Franz <kfranz@23andme.com>
2015-04-17 05:38:46 -0700

    more git help

--- git/build.sh
+++ git/build.sh
@@ -3,9 +3,18 @@ make configure
 
 ./configure --help
 
+
+echo "$PREFIX"
+pwd
+ls -al
+
+export PERL5LIB=$PREFIX
+
 ./configure \
     --with-zlib=$PREFIX \
+    --with-python=$PYTHON \
     --with-openssl=$PREFIX \
     --prefix=$PREFIX
+    # --with-curl
 make all doc
 make install install-doc
