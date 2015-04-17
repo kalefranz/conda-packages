5ebc8b302d2088e20e6e609a3340438fd1f1ebb5
Kale Franz <kfranz@23andme.com>
2015-04-17 06:56:47 -0700

    git wtf

--- git/build.sh
+++ git/build.sh
@@ -4,18 +4,13 @@ make configure
 ./configure --help
 
 
-# echo "$PREFIX"
-# pwd
-# ls -al
-
-# export PERL5LIB=$LIBRARY_PATH/perl5/5.18.2:$LIBRARY_PATH/perl5/5.18.2/x86_64-linux
-
 ./configure \
     --with-zlib=$PREFIX \
     --with-python=$PYTHON \
     --with-openssl=$PREFIX \
-    --without-perl \
-    --without-tcltk \
     --prefix=$PREFIX
+    # --without-perl \
+    # --without-tcltk \
+
 make all doc NO_PERL=YesPlease NO_TCLTK=YesPlease
 make install install-doc
