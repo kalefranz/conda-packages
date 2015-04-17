51e0063d20503585ba1b4b1ad771f269255ff127
Kale Franz <kfranz@23andme.com>
2015-04-17 09:35:11 -0700

    mysql-client debug

--- mysql-client/build.sh
+++ mysql-client/build.sh
@@ -10,7 +10,11 @@ export LD_LIBRARY_PATH=$LIBRARY_PATH
 # 5.5.x
 ############
 if [[ $PKG_VERSION == 5.5* ]]; then
+    cmake -LH
+
     cmake . \
+        -DCMAKE_INCLUDE_PATH=$INCLUDE_PATH \
+        -DCMAKE_LIBRARY_PATH=$LIBRARY_PATH \
         -DCMAKE_INSTALL_PREFIX=$PREFIX \
         -DCMAKE_VERBOSE_MAKEFILE=ON \
         -DWITH_UNIT_TESTS=OFF \
@@ -26,6 +30,7 @@ fi
 # 5.1.x
 ############
 if [[ $PKG_VERSION == 5.1* ]]; then
+    ./configure --help
     ./configure \
         --without-server \
         --prefix=$PREFIX
