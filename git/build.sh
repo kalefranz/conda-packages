96c67213ec9de423529223339333898051f0c425
Kale Franz <kfranz@23andme.com>
2015-04-17 07:44:42 -0700

    iotimeout wrong place

--- git/build.sh
+++ git/build.sh
@@ -3,16 +3,11 @@ make configure
 
 ./configure --help
 
-
 ./configure \
     --with-zlib=$PREFIX \
     --with-python=$PYTHON \
     --with-openssl=$PREFIX \
     --prefix=$PREFIX
-
-export NO_PERL=YesPlease
-export NO_TCLTK=YesPlease
-export NO_GETTEXT=YesPlease
 
 make all NO_PERL=YesPlease NO_TCLTK=YesPlease NO_GETTEXT=YesPlease
 make install NO_PERL=YesPlease NO_TCLTK=YesPlease NO_GETTEXT=YesPlease
