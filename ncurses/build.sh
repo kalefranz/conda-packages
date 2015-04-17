ab05510c2de3639da039998be33524a0e0ac1a27
Kale Franz <kfranz@23andme.com>
2015-04-17 08:14:25 -0700

    no ncurses tests

--- ncurses/build.sh
+++ ncurses/build.sh
@@ -3,8 +3,14 @@
 mkdir $PREFIX/lib
 
 sh ./configure --prefix=$PREFIX \
-    --without-debug --without-ada --without-manpages --without-progs \
-    --with-shared --disable-overwrite --with-termlib=tinfo --enable-widec
+    --without-debug \
+    --without-ada \
+    --without-manpages \
+    --without-progs \
+    --with-shared \
+    --disable-overwrite \
+    --with-termlib=tinfo \
+    --enable-widec
 
 make -j$(getconf _NPROCESSORS_ONLN)
 make install
