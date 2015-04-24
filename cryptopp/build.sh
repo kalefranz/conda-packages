ad0f9954ce4327e37df89619dd725b8d287b7773
Kale Franz <kfranz@23andme.com>
2015-04-24 06:54:07 -0700

    fix cryptopp

--- cryptopp/build.sh
+++ cryptopp/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eu
 
-sed 's/CXXFLAGS = -DNDEBUG -g -O2/CXXFLAGS = -DNDEBUG -g -O2 -fPIC/' GNUmakefile
+sed -i 's/CXXFLAGS = -DNDEBUG -g -O2/CXXFLAGS = -DNDEBUG -g -O2 -fPIC/' GNUmakefile
+
+head GNUmakefile
+
 make
 make install PREFIX=$PREFIX
