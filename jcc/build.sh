30be62a06ac7411f4ac83ac5765bbb36197ddfed
Kale Franz <kfranz@23andme.com>
2015-04-24 13:55:28 -0700

    set JCC_JDK

--- jcc/build.sh
+++ jcc/build.sh
@@ -3,6 +3,8 @@
 export JAVA_HOME=$(readlink -f $PREFIX/bin/java | sed "s:/bin/java::")
 echo $JAVA_HOME
 
+export JCC_JDK=$JAVA_HOME
+
 # JCC_JDK
 # Java JDK directory '/usr/lib/jvm/java-7-openjdk-amd64' does not exist.
 
