590d68f1e4fe3f8bf504ba20dfb386090b29262d
Kale Franz <kfranz@23andme.com>
2015-04-24 13:00:08 -0700

    build it!

--- java-jdk/build.sh
+++ java-jdk/build.sh
@@ -1,34 +1,12 @@
 #/bin/bash -eu
 
-printenv
-pwd
-ls -al
-
-
-# BUILD_CACHE="$RECIPE_DIR/../build/cache"
-# mkdir -p $BUILD_CACHE
-
-
-# # Download
-# if [[ ! -f $BUILD_CACHE/$JAVA_JDK_FN ]]; then
-#     curl -L -C - \
-#         --referer http://www.azulsystems.com/products/zulu/downloads \
-#         -o $BUILD_CACHE/$JAVA_JDK_FN \
-#         $JAVA_JDK_URL
-# fi
-# cp -v $BUILD_CACHE/$JAVA_JDK_FN $JAVA_JDK_FN
-
-
 # # this must exist because ln does not have the -r option in Mac. Apple, unix - but not!
 relpath(){ python -c "import os.path; print(os.path.relpath('$1','${2:-$PWD}'))" ; }
 LINKLOC="$PREFIX/lib/*/jli"
 
-
 # clean up
 rm -rf release README readme.txt Welcome.html *jli.* demo sample *.zip
 mv * $PREFIX
-ls -al $PREFIX
-ls -al $PREFIX/bin
 
 # Install
 JLI_REL=$(relpath $LINKLOC/*jli.* $PREFIX/lib)
