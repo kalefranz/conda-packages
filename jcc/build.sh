7e12b07adfba2b91b165092f2804e1885f3211c1
Kale Franz <kfranz@23andme.com>
2015-04-24 14:03:25 -0700

    poking at bugs

--- jcc/build.sh
+++ jcc/build.sh
@@ -1,7 +1,11 @@
 #/bin/bash -eu
 
 export JAVA_HOME=$(readlink -f $PREFIX/bin/java | sed "s:/bin/java::")
-echo $JAVA_HOME
+
+export JRE_HOME=$JAVA_HOME/jre
+export CLASSPATH=.:$JAVA_HOME/lib:$JRE_HOME/lib:$CLASSPATH
+export PATH=$JAVA_HOME/bin:$JRE_HOME/bin:$PATH
+
 
 export JCC_JDK=$JAVA_HOME
 
