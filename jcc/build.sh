e0c7702e4df9f86f2ca6f1d6c2a74f240b1416c7
Kale Franz <kfranz@23andme.com>
2015-04-24 13:52:53 -0700

    set JAVA_HOME

--- jcc/build.sh
+++ jcc/build.sh
@@ -1,5 +1,9 @@
 #/bin/bash -eu
 
-export JAVA_HOME=$(readlink -f `which java` | sed "s:/bin/java::")
+export JAVA_HOME=$(readlink -f $PREFIX/bin/java | sed "s:/bin/java::")
+echo $JAVA_HOME
+
+# JCC_JDK
+# Java JDK directory '/usr/lib/jvm/java-7-openjdk-amd64' does not exist.
 
 $PYTHON setup.py install
