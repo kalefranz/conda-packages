901b845db475d4d8e307a43b5bd34c9e8358380d
Kale Franz <kfranz@23andme.com>
2015-04-24 13:59:05 -0700

    poking at bugs

--- jcc/build.sh
+++ jcc/build.sh
@@ -8,4 +8,15 @@ export JCC_JDK=$JAVA_HOME
 # JCC_JDK
 # Java JDK directory '/usr/lib/jvm/java-7-openjdk-amd64' does not exist.
 
+
+#/opt/miniconda/envs/_build/jre/lib/amd64/libnio.so
+
+echo $PREFIX
+ls -al $PREFIX
+
+echo $JAVA_HOME
+ls -al $JAVA_HOME
+
+ls -al $JAVA_HOME/jre/lib/amd64
+
 $PYTHON setup.py install
