aaa4ea0441749d0402c4ce67fdd4ee0a9a1d0bda
Kale Franz <kfranz@23andme.com>
2015-04-24 08:16:47 -0700

    disable perl

--- net-snmp/build.sh
+++ net-snmp/build.sh
@@ -1,8 +1,13 @@
 #/bin/bash -eu
 
+# notes: http://www.net-snmp.org/docs/INSTALL.html
+
 ./configure --help
 
-./configure --prefix=$PREFIX
+./configure \
+    --prefix=$PREFIX \
+    --disable-embedded-perl \
+    --enable-shared
 make
 make install
     
