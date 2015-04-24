0affea6ab6e8e33a129598b86c35a860779048f5
Kale Franz <kfranz@23andme.com>
2015-04-24 15:32:56 -0700

    try jcc with different java

--- jcc/build.sh
+++ jcc/build.sh
@@ -6,15 +6,8 @@ export JRE_HOME=$JAVA_HOME/jre
 export CLASSPATH=.:$JAVA_HOME/lib:$JRE_HOME/lib:$CLASSPATH
 export PATH=$JAVA_HOME/bin:$JRE_HOME/bin:$PATH
 
-export JCC_JDK=$JAVA_HOME
+export JCC_JDK=$JAVA_HOME:$JAVA_HOME/jre
 
-# JCC_JDK
-# Java JDK directory '/usr/lib/jvm/java-7-openjdk-amd64' does not exist.
-
-
-#/opt/miniconda/envs/_build/jre/lib/amd64/libnio.so
-ls -al /opt/miniconda/envs/_build/jre/lib/amd64
-ls -al /opt/miniconda/envs/_build/jre/lib/amd64/libnio.so
-
-$PYTHON setup.py build
+#$PYTHON setup.py build
 $PYTHON setup.py install
+
