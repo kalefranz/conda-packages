ab6eb346e1db6d5812ce77f47b4d2c8f09fe1433
Kale Franz <kfranz@23andme.com>
2015-04-30 22:43:55 -0700

    debug

--- bazaarvoice/build.sh
+++ bazaarvoice/build.sh
@@ -1,16 +1,18 @@
 #/bin/bash -eu
 
 
-pwd
-ls -al
-cd $PREFIX
-pwd
-ls -al
+# pwd
+# ls -al
+# cd $PREFIX
+# pwd
+# ls -al
 
-ls -al /opt/miniconda/envs/_build
+# ls -al /opt/miniconda/envs/_build
 
-printenv
+# printenv
 
 # cd $SRC_DIR
+# RECIPE_DIR=/home/binstar/source/bazaarvoice
+# SRC_DIR=/opt/miniconda/conda-bld/work
 
 $PYTHON setup.py install
