c89dcc6581f7465f6b578a96db4ed08ad6397133
Kale Franz <kfranz@23andme.com>
2015-05-01 12:09:49 -0700

    add mod_python to httpd

--- httpd/build.sh
+++ httpd/build.sh
@@ -5,12 +5,25 @@ curl http://mirrors.gigenet.com/apache/apr/apr-1.5.2.tar.gz | tar -xz -C $PREFIX
 curl http://mirrors.gigenet.com/apache/apr/apr-util-1.5.4.tar.gz | tar -xz -C $PREFIX/srclib/apr-util
 
 
+http://dist.modpython.org/dist/
+
 ./configure --help
 
 ./configure \
     --prefix=$PREFIX \
     --with-included-apr
+make
+make install
 
+
+## mod_python ##
+mkdir mod_python
+pushd mod_python
+curl http://dist.modpython.org/dist/mod_python-3.4.1.tgz | tar -xz
+./configure --help
+./configure \
+    --prefix=$PREFIX \
+    --with-python=$PYTHON
 make
 make install
 
