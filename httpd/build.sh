dd280a3a7e03580ab4376570eecd8d9a997bab28
Kale Franz <kfranz@23andme.com>
2015-05-01 12:30:11 -0700

    mkdir

--- httpd/build.sh
+++ httpd/build.sh
@@ -1,7 +1,9 @@
 #/bin/bash -eu
 
 # Download APR
+mkdir -p $PREFIX/srclib/apr
 curl http://mirrors.gigenet.com/apache/apr/apr-1.5.2.tar.gz | tar -xz -C $PREFIX/srclib/apr
+mkdir -p $PREFIX/srclib/apr-util
 curl http://mirrors.gigenet.com/apache/apr/apr-util-1.5.4.tar.gz | tar -xz -C $PREFIX/srclib/apr-util
 
 
