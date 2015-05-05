eb39e5b3e88f708f2e911f495c528eae30851ddf
Kale Franz <kfranz@23andme.com>
2015-05-05 02:16:11 -0700

    no rake

--- ttam-gems/build.sh
+++ ttam-gems/build.sh
@@ -6,6 +6,6 @@ gem install compass -v 0.12.2
 gem install fssm -v 0.2.9
 gem install minitest -v 4.3.3
 gem install modular-scale -v 1.0.6
-gem install rake -v 10.0.3
+# gem install rake -v 10.0.3
 gem install sass -v 3.2.4
 gem install sassy-math -v 1.5
