92cfc98f2ea817b513b68f7385f1dbdc1f3de1e3
Kale Franz <kfranz@23andme.com>
2015-05-04 17:59:01 -0700

    specify versions

--- ttam-gems/build.sh
+++ ttam-gems/build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -eu
 
-gem install \
-    compass:0.12.1 \
-    modular-scale:1.0.6 \
-    sass:3.2.0 \
-    sassy-math:1.5 \
-    zurb-foundation:3.2.5 \
-    chunky_png:1.2.6 \
-    fssm:0.2.9 \
-    rake:10.0.3
+gem install rake -v 10.0.3
+gem install compass -v 0.12.1
+gem install modular-scale -v 1.0.6
+gem install sass -v 3.2.0
+gem install sassy-math -v 1.5
+gem install zurb-foundation -v 3.2.5
+gem install chunky_png -v 1.2.6
+gem install fssm -v 0.2.9
