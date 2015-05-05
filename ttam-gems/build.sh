787d93ce77fb255a5d7a592b600759d71f93cb4e
Kale Franz <kfranz@23andme.com>
2015-05-05 01:31:38 -0700

    rearrange install

--- ttam-gems/build.sh
+++ ttam-gems/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -eu
 
-gem install rake -v 10.0.3
-gem install compass -v 0.12.2
-gem install modular-scale -v 1.0.6
-gem install sass -v 3.2.4
-gem install sassy-math -v 1.5
 gem install zurb-foundation -v 3.2.5
 gem install chunky_png -v 1.2.6
+gem install compass -v 0.12.2
 gem install fssm -v 0.2.9
+gem install minitest -v 4.3.3
+gem install modular-scale -v 1.0.6
+gem install rake -v 10.0.3
+gem install sass -v 3.2.4
+gem install sassy-math -v 1.5
