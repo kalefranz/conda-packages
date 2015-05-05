4ccdf253c8f382e6c38c2cf780cf29dab6e58d85
Kale Franz <kfranz@23andme.com>
2015-05-04 17:39:36 -0700

    ttam-gems

---
+++ ttam-gems/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -eu
+
+gem install \
+    compass:0.12.1 \
+    modular-scale:1.0.6 \
+    sass:3.2.0 \
+    sassy-math:1.5 \
+    zurb-foundation:3.2.5 \
+    chunky_png:1.2.6 \
+    fssm:0.2.9 \
+    rake:10.0.3
