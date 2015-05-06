37563c54e604c69be8a3476331e49837d462acf3
Kale Franz <kfranz@23andme.com>
2015-05-06 16:30:09 -0700

    update ttam-gems version and order

--- ttam-gems/build.sh
+++ ttam-gems/build.sh
@@ -1,11 +1,22 @@
 #!/bin/bash -eu
 
-gem install zurb-foundation -v 3.2.5
-gem install chunky_png -v 1.2.6
 gem install compass -v 0.12.2
-gem install fssm -v 0.2.9
-gem install minitest -v 4.3.3
 gem install modular-scale -v 1.0.6
-# gem install rake -v 10.0.3
-gem install sass -v 3.2.4
+gem install rake
+gem install sass -v 3.2.19
 gem install sassy-math -v 1.5
+gem install zurb-foundation -v 3.2.5
+
+
+# gem install chunky_png -v 1.2.6
+# gem install fssm -v 0.2.9
+# gem install minitest -v 4.3.3
+# # gem install rake -v 10.0.3
+# # gem install sass -v 3.2.4
+
+
+# ruby 2.1.0p0 (2013-12-25 revision 44422) [x86_64-darwin12.0]
+# modular-scale (1.0.6)
+# compass (0.13.alpha.12, 0.12.2)
+# sass (3.3.0.rc.2, 3.2.13)
+# sassy-math (1.5)
