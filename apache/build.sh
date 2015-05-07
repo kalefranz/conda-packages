3306e5bef5f0b0266c2b58b0534b4d8eac942c7a
Kale Franz <kfranz@23andme.com>
2015-05-07 00:42:13 -0700

    apache

---
+++ apache/build.sh
@@ -0,0 +1,13 @@
+#/bin/bash -eu
+
+# install httpd
+$PYTHON setup.py intall
+
+
+# install mod_wsgi
+MOD_WSGI_URL=https://pypi.python.org/packages/source/m/mod_wsgi/mod_wsgi-4.4.11.tar.gz
+
+mkdir mod-wsgi
+curl "$MOD_WSGI_URL" | tar -xz -C mod-wsgi --strip-components=1
+pushd mod-wsgi
+$PYTHON setup.py intall
