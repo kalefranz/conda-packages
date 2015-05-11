205ad421f0c2bd78d35601b6bb04439ae2889ef5
Kale Franz <kfranz@23andme.com>
2015-05-11 16:50:57 -0700

    enable caches

--- httpd/build.sh
+++ httpd/build.sh
@@ -10,6 +10,7 @@ curl https://archive.apache.org/dist/apr/apr-util-1.5.4.tar.gz | tar -xz -C $SRC
 
 ./configure \
     --prefix=$PREFIX \
+    --enable-cache \
     --enable-disk-cache \
     --enable-file-cache \
     --enable-mem-cache \
