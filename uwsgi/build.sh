0e13a156e7a07ff591e514868a82c73a78fe9112
Kale Franz <kfranz@23andme.com>
2015-05-12 07:56:47 -0700

    uwsgi debug

--- uwsgi/build.sh
+++ uwsgi/build.sh
@@ -3,15 +3,4 @@
 UWSGI_INCLUDES="${PREFIX}/include,${PREFIX}/openssl/include" \
     $PYTHON uwsgiconfig.py --build
 
-pwd
-ls -al
-ls -al $PREFIX/bin
-ls -al $PREFIX/lib
-
-cat uwsgibuild.log
-
-ls -al core
-ls -al lib
-ls -al proto
-
-
+mv uwsgi $PREFIX/bin
