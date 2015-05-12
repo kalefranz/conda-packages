75eb4dbc6d0fd068118f56098a2576c4e501ca67
Kale Franz <kfranz@23andme.com>
2015-05-11 18:41:31 -0700

    new link

--- mod_wsgi/post-link.sh
+++ mod_wsgi/post-link.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -eu
 
-ln -s $SP_DIR/mod_wsgi/server/mod_wsgi-py27.so $PREFIX/modules/mod_wsgi.so
+ln -s $PREFIX/lib/python2.7/site-packages/mod_wsgi/server/mod_wsgi-py27.so $PREFIX/modules/mod_wsgi.so
