84847bc5f1e7099aa4923810daa11f8cc879980c
Kale Franz <kfranz@23andme.com>
2015-05-15 02:07:44 -0700

    more redis tinkering

--- redis/build.sh
+++ redis/build.sh
@@ -2,10 +2,13 @@
 
 unset ARCH  # https://github.com/redis/hiredis-rb/issues/2
 
-pushd deps
-make hiredis jemalloc linenoise lua
+# pushd deps
+# make hiredis jemalloc linenoise lua
 
-popd
-make
-# make test
-make install
+# popd
+
+make distclean
+
+# make
+
+make PREFIX=$PREFIX install
