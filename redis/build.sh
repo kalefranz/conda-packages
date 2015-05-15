e698c16fd86dc96eb56f5e73cf31da1bf9169413
Kale Franz <kfranz@23andme.com>
2015-05-15 03:34:24 -0700

    redis with passing tests!

--- redis/build.sh
+++ redis/build.sh
@@ -1,14 +1,5 @@
 #!/bin/bash -eu
 
 unset ARCH  # https://github.com/redis/hiredis-rb/issues/2
-
-# pushd deps
-# make hiredis jemalloc linenoise lua
-
-# popd
-
 make distclean
-
-# make
-
 make PREFIX=$PREFIX install
