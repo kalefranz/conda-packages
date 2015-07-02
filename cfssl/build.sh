b3f2b7582caaaa4c84a440d84322da3b33782cac
Kale Franz <kfranz@23andme.com>
2015-07-01 19:52:44 -0700

    fix ellipses

--- cfssl/build.sh
+++ cfssl/build.sh
@@ -4,6 +4,7 @@ export GOROOT=$PREFIX
 # export GOPATH=$PREFIX
 
 #go get -u github.com/cloudflare/cfssl/cmd/cfssl
-go get $SRC_DIR/cmd/...
-
+go get $SRC_DIR/cmd/cfssl
+go get $SRC_DIR/cmd/cfssljson
+go get $SRC_DIR/cmd/mkbundle
 
