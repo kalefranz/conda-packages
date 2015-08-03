b5655cbbf7293fa9bb990b97db6fc183cfd43ad7
Kale Franz <kfranz@23andme.com>
2015-08-03 14:11:13 -0700

    update anaconda-clone script

--- go/go/build.sh
+++ go/go/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# https://golang.org/src/make.bash
 export GOROOT=$PREFIX
-export GOROOT_FINAL=$PREFIX
+export GOROOT_FINAL=/usr/conda
 export GOBIN=$GOROOT/bin
-
+export CGO_ENABLED=1
+export GO_EXTLINK_ENABLED=0
+export GO_DISTFLAGS="-s"
+export GO_CCFLAGS="-static"
 
 pwd
 ls -al
