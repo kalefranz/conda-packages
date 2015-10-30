8961dd249ba3c1d041e535e21f53ef28cf09bb15
Kale Franz <kfranz@23andme.com>
2015-10-30 07:57:52 -0700

    node update

--- centos5/node/build.sh
+++ centos5/node/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -eu
 
+./configure --help
+
 $PYTHON ./configure --prefix=$PREFIX
+
+#    --shared-openssl \
+#    --shared-openssl-includes=$PREFIX/include \
+#    --shared-zlib \
+#    --shared-zlib-includes=$PREFIX/include
+
 make
 make install
-
+make test
