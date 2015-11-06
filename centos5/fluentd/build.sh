b22a934b0906496a6f69d77c5fa94054fd5a85df
David Cheney <dcheney@23andme.com>
2015-11-05 18:13:52 -0800

    build sumologic plugin from forked repo

--- centos5/fluentd/build.sh
+++ centos5/fluentd/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -eu
 gem install fluentd --no-ri --no-rdoc -v $PKG_VERSION
-gem install fluent-plugin-sumologic
+git clone https://github.com/dgc-wh/fluent-plugin-sumologic.git
+cd fluent-plugin-sumologic
+gem build fluent-plugin-sumologic.gemspec 
+gem install fluent-plugin-sumologic-0.0.1.gem
 
