21129eb41f198e7c67bb5b4e167940aea4c2b47c
Kale Franz <kfranz@23andme.com>
2015-11-05 19:07:54 -0800

    compile a good ruby 2.2

---
+++ centos5/ruby2/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -eu
+
+./configure --help
+
+./configure \
+        --prefix=$PREFIX \
+        --enable-load-relative \
+        --disable-install-doc \
+        --enable-shared \
+        --enable-rpath \
+        --with-jemalloc \
+        --with-opt-dir=$PREFIX \
+        --with-libyaml-dir=$PREFIX \
+        --with-openssl-dir=$PREFIX \
+        --with-readline-dir=$PREFIX \
+        --with-zlib-dir=$PREFIX
+
+make
+make install
+
