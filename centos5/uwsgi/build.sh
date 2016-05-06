ca97d91ba35f5b759e7f7072ee41ca23b4ee69b0
Kale Franz <kale@franz.io>
2016-05-06 00:39:21 -0500

    uwsgi

--- centos5/uwsgi/build.sh
+++ centos5/uwsgi/build.sh
@@ -1,6 +1,5 @@
-#/bin/bash -eu
+export UWSGI_PROFILE="$RECIPE_DIR/uwsgi_config.ini"
+export UWSGI_INCLUDES="$PREFIX/include,$PREFIX/include/openssl"
+export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
 
-UWSGI_INCLUDES="${PREFIX}/include,${PREFIX}/openssl/include" \
-    LDFLAGS="-L${PREFIX}/lib" \
-    $PYTHON setup.py install
-
+$PYTHON setup.py install --single-version-externally-managed --record record.txt
