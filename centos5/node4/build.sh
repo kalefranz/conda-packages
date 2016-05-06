84274bbe5ca1ff846f9080f0cb0931b5a369c594
Kale Franz <kale@franz.io>
2016-05-06 00:45:14 -0500

    trying node4 for osx

--- centos5/node4/build.sh
+++ centos5/node4/build.sh
@@ -2,21 +2,51 @@
 
 ./configure --help
 
-export CC="$PREFIX/bin/gcc"
-export CFLAGS="-I$PREFIX/include"
-export CPPFLAGS="-I$PREFIX/include"
 
-$PYTHON ./configure --prefix=$PREFIX \
-    --partly-static \
-    --shared-openssl \
-    --shared-openssl-includes $PREFIX/include \
-    --shared-openssl-libpath $PREFIX/lib \
-    --shared-zlib \
-    --shared-zlib-includes $PREFIX/include \
-    --shared-zlib-libpath $PREFIX/lib \
-    --without-snapshot
+configure_linux() {
+    export CC="$PREFIX/bin/gcc"
+    export CFLAGS="-I$PREFIX/include"
+    export CPPFLAGS="-I$PREFIX/include"
 
-# disabling snapshots per thread https://github.com/nodejs/node/issues/4212#issuecomment-163794906
+    $PYTHON ./configure --prefix=$PREFIX \
+        --partly-static \
+        --shared-libuv \
+        --shared-libuv-includes $PREFIX/include \
+        --shared-libuv-libpath $PREFIX/lib \
+        --shared-openssl \
+        --shared-openssl-includes $PREFIX/include \
+        --shared-openssl-libpath $PREFIX/lib \
+        --shared-zlib \
+        --shared-zlib-includes $PREFIX/include \
+        --shared-zlib-libpath $PREFIX/lib \
+        --without-snapshot
+
+}
+
+configure_darwin() {
+    export CC="$PREFIX/bin/gcc"
+    export CFLAGS="-I$PREFIX/include"
+    export CPPFLAGS="-I$PREFIX/include"
+
+    $PYTHON ./configure --prefix=$PREFIX \
+        --shared-libuv \
+        --shared-libuv-includes $PREFIX/include \
+        --shared-libuv-libpath $PREFIX/lib \
+        --shared-openssl \
+        --shared-openssl-includes $PREFIX/include \
+        --shared-openssl-libpath $PREFIX/lib \
+        --shared-zlib \
+        --shared-zlib-includes $PREFIX/include \
+        --shared-zlib-libpath $PREFIX/lib \
+        --without-snapshot
+}
+
+
+case $(uname -s) in
+    Darwin) configure_darwin ;;
+    Linux) configure_linux ;;
+esac
+
 
 make
 make install
